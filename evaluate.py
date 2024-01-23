7ea274a629b2aeb3d5028f2a852d75a5608343c3
조신영(mac) <>
2024-01-23 19:51:17 +0900

    refactor opt in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,105 +1,16 @@
-import os
-import argparse
 import numpy as np
+import torch
+import os
 import csv
+
 import matplotlib.pyplot as plt
-
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, accuracy_score, RocCurveDisplay
-import torch
-
 
 from models.mdmcgan import Generator as mdmcgan_gen
 from models.cond_wgan_gp import Generator as cond_wgan_gp_gen
+from train_params import opt
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--n_epochs", type=int, default=500, help="number of epochs of training"
-)
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.00005, help="adam: learning rate")
-parser.add_argument(
-    "--b1",
-    type=float,
-    default=0.5,
-    help="adam: decay of first order momentum of gradient",
-)
-parser.add_argument(
-    "--b2",
-    type=float,
-    default=0.999,
-    help="adam: decay of first order momentum of gradient",
-)
-parser.add_argument(
-    "--n_cpu",
-    type=int,
-    default=8,
-    help="number of cpu threads to use during batch generation",
-)
-parser.add_argument(
-    "--latent_dim", type=int, default=100, help="dimensionality of the latent space"
-)
-parser.add_argument(
-    "--feature_size", type=int, default=128, help="size of each feature dimension"
-)
-parser.add_argument("--feature_num", type=int, default=3, help="number of features")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument(
-    "--n_critic",
-    type=int,
-    default=5,
-    help="number of training steps for discriminator per iter",
-)
-parser.add_argument(
-    "--clip_value",
-    type=float,
-    default=0.01,
-    help="lower and upper clip value for disc. weights",
-)
-parser.add_argument(
-    "--sample_interval", type=int, default=400, help="interval betwen image samples"
-)
-parser.add_argument(
-    "--dataset",
-    type=str,
-    choices=["mnist", "fashion", "mnist_c"],
-    default="uci_har",
-    help="dataset to use",
-)
-parser.add_argument(
-    "--num_modalities",
-    type=int,
-    default=2,
-    help="number of modalities",
-)
-parser.add_argument(
-    "--non_iid",
-    action="store_true",
-    help="use non-iid dataset",
-)
-parser.add_argument(
-    "--no-non_iid",
-    dest="non_iid",
-    action="store_false",
-    help="use iid dataset",
-)
-parser.add_argument(
-    "--algorithm",
-    type=str,
-    choices=["mdmcgan", "cond_wgan_gp", "orig_cond_wgan_gp"],
-    default="mdmcgan",
-    help="feature generating algorithm to use",
-)
-parser.set_defaults(non_iid=True)
-parser.add_argument("--remove_labels_num", type=int, default=3, choices=[1, 2, 3])
-parser.add_argument("--filter_ratio", type=float, default=0.9)
-
-opt = parser.parse_args()
-
-opt.feature_shape = (opt.channels, opt.feature_size, opt.feature_num)
-opt.n_classes = 6
 
 os.makedirs("plots", exist_ok=True)
 
